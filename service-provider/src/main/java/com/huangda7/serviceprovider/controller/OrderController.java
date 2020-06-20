c72366b9c9d75c021f4101ab1ef3449c3410f4e9
hzghzg <2452376183@qq.com>
2020-06-20 15:23:42 +0800

    add order controller and service

--- service-provider/src/main/java/com/huangda7/serviceprovider/controller/OrderController.java
+++ service-provider/src/main/java/com/huangda7/serviceprovider/controller/OrderController.java
@@ -1,8 +1,17 @@
 package com.huangda7.serviceprovider.controller;
 
+import com.huangda7.serviceprovider.common.ResponseUtil;
+import com.huangda7.serviceprovider.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderController {
+    @Autowired
+    OrderService orderService;
 
+    public void getOrderInformation() {
+        String orderInformation = orderService.getOrderInformation();
+        ResponseUtil.responseSuccess(orderInformation);
+    }
 }
---
+++ service-provider/src/main/java/com/huangda7/serviceprovider/service/OrderService.java
@@ -0,0 +1,11 @@
+package com.huangda7.serviceprovider.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+    public String getOrderInformation(){
+        String orderInformation = "远程商品信息";
+        return orderInformation;
+    }
+}
