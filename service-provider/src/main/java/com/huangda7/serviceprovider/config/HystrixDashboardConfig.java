04116a7ddb8cbe2390ec5f95273e312462e486bf
hzghzg <2452376183@qq.com>
2020-07-23 21:20:20 +0800

    add hystrix dashboard

---
+++ service-provider/src/main/java/com/huangda7/serviceprovider/config/HystrixDashboardConfig.java
@@ -0,0 +1,16 @@
+package com.huangda7.serviceprovider.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HystrixDashboardConfig {
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet() {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        servletRegistrationBean.addUrlMappings("/actuator/hystrix.stream");
+        return servletRegistrationBean;
+    }
+}
