292b6a550c9871b92f7be22a17cc637f4a6a8ac6
hzghzg <2452376183@qq.com>
2020-07-29 21:05:09 +0800

    add log

--- service-provider/src/main/java/com/huangda7/serviceprovider/controller/HelloController.java
+++ service-provider/src/main/java/com/huangda7/serviceprovider/controller/HelloController.java
@@ -1,13 +1,19 @@
 package com.huangda7.serviceprovider.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
     @GetMapping(path = "/hello")
     public String sayHello(){
+        logger.debug("debug--接口被调用");
+        //默认是info级别，info级别的才会被调用
+        logger.info("info--接口被调用");
         return "hello";
     }
 }
